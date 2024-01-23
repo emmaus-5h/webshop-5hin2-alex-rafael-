18a70e53e9c286a8074e2216756b23cb88db8062
RafaelTulling <115073456+RafaelTulling@users.noreply.github.com>
2024-01-23 09:00:11 +0000

    product toegevoegd

--- db/create.sql
+++ db/create.sql
@@ -21,7 +21,7 @@ CREATE TABLE products (
 --
 
 insert into products (name, description, code, price) values ('Unisex Oversized BMW Hoodie', 'Blaas nieuw leven in een klassiek kledingstuk met je eigen kunst. Deze hoodie heeft een uniseks ontwerp mmet zijnaden waadoor het kledingstuk op de lange termijn zijn vorm houdt. Met een ruime kangoeroezak voor meer dagelijkse bruikbaarheid, een comfortabele pasvorm op maat en meerdere kleuren om uit te kiezen - deze hoodies bieden een gezellige draagervaring boordevol stijl.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
+insert into products (name, description, code, price) values ('Duffel Bag BMW', 'Licht en sterk, onze custom gemaakte tas is een geweldige manier om jouw dag makkelijker te maken. Perfect voor de sportschool of een dagtripje. Deze tas is gemakkelijk te dragen en zijn klaar voor elk terrein.', '077030122-3', 11);
 insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
 insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
 insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
